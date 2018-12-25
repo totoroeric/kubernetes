ab18fba736942f0bb2eb7e8c8a4f041ba42cd272
totoroeric <544107702@qq.com>
2018-12-25 12:27:24 +0000

    heapster

---
+++ pull-and-tag-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+images=(kube-proxy-amd64:v1.9.0 kube-scheduler-amd64:v1.9.0 kube-controller-manager-amd64:v1.9.0 kube-apiserver-amd64:v1.9.0 etcd-amd64:3.1.10 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.8.3 k8s-dns-sidecar-amd64:1.14.7 k8s-dns-kube-dns-amd64:1.14.7 k8s-dns-dnsmasq-nanny-amd64:1.14.7) 
+for image in ${images[@]} ; do 
+	docker pull shayu/$image 
+	docker tag shayu/$image gcr.io/google_containers/$image 
+	docker rmi shayu/$image 
+done
+
